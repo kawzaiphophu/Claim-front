c0bd70b069d9ae5d53bc4d2d87e073750bf965c5
kawzaiphophu <156281540+kawzaiphophu@users.noreply.github.com>
2024-04-08 17:53:50 +0700

    fix navbar close

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -15,32 +15,32 @@ function Nav() {
   }, []);
 
   useEffect(() => {
-      const sections = document.querySelectorAll("section");
-      const navs = document.querySelectorAll(".navbar-nav li a");
-      const intersectionHandler = (entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            const id = entry.target.getAttribute('id');
-            const navLink = document.querySelector('.navbar-nav li a[data-id="' + id + '"]');
-            if (navLink) {
-              navs.forEach(nav => nav.classList.remove('active'));
-              navLink.classList.add('active');
-            } else {
-              navs.forEach(nav => nav.classList.remove('active'));
-            }
+    const sections = document.querySelectorAll("section");
+    const navs = document.querySelectorAll(".navbar-nav li a");
+    const intersectionHandler = (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const id = entry.target.getAttribute('id');
+          const navLink = document.querySelector('.navbar-nav li a[data-id="' + id + '"]');
+          if (navLink) {
+            navs.forEach(nav => nav.classList.remove('active'));
+            navLink.classList.add('active');
+          } else {
+            navs.forEach(nav => nav.classList.remove('active'));
           }
-        });
-      };
-  
-      const observer = new IntersectionObserver(intersectionHandler, {
-        rootMargin: '-120px 120px -80% 0px',
+        }
       });
-      sections.forEach(sec => {
-        observer.observe(sec);
-      });
-      return () => {
-        observer.disconnect();
-      };
+    };
+
+    const observer = new IntersectionObserver(intersectionHandler, {
+      rootMargin: '-120px 120px -80% 0px',
+    });
+    sections.forEach(sec => {
+      observer.observe(sec);
+    });
+    return () => {
+      observer.disconnect();
+    };
   }, [scrollY]);
 
 
@@ -51,6 +51,12 @@ function Nav() {
       window.scrollTo({ top: offset, behavior: 'smooth' });
     }
   };
+  const closeNavbar = () => {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarToggler.classList.add('collapsed');
+    navbarCollapse.classList.remove('show');
+  };
 
 
   return (
@@ -60,41 +66,46 @@ function Nav() {
           <img src="https://cdn-icons-png.freepik.com/256/10137/10137151.png?ga=GA1.1.1207387130.1709617310&" alt="" />
         </Link>
         <button className="navbar-toggler"
-          type="button" data-bs-toggle="collapse"
+          type="button"
+          data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
-          aria-expanded="false" aria-label="Toggle navigation">
+          aria-expanded="false"
+          aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <nav className="collapse navbar-collapse"
           id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-lg-0">
             <li className="nav-item">
-              <Link data-id="home" className="nav-link mx-3 active" to="/" onClick={() => { scrollTo('home');}}>Home</Link>
+              <Link data-id="home" className="nav-link mx-3 active" to="/" onClick={() => { scrollTo('home'); closeNavbar(); }}>Home</Link>
             </li>
             <li >
-              <Link data-id="about" className="nav-link mx-3" to="/" onClick={() => { scrollTo('about');}}>About</Link>
+              <Link data-id="about" className="nav-link mx-3" to="/" onClick={() => { scrollTo('about'); closeNavbar(); }}>About</Link>
             </li>
             <li className="nav-item dropdown">
               <Link className="nav-link mx-3"
                 data-id="project"
-                to="/" id="navbarDropdown"
+                to="/"
+                id="navbarDropdown"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
-                onClick={() => scrollTo('project')}>
+                onClick={() => { scrollTo('project'); }}>
                 Project
               </Link>
               <ul className="dropdown-menu w-25 px-3 "
                 aria-labelledby="navbarDropdown">
                 <li ><Link className="dropdown-item py-2"
-                  to="/ClaimList"> Claim</Link></li>
+                  to="/ClaimList"
+                  onClick={()=>{closeNavbar(); }}> Claim</Link></li>
                 <li><Link className="dropdown-item py-2"
-                  to="/Pokemon"> Poke</Link></li>
+                  to="/Pokemon"
+                  onClick={()=>{closeNavbar(); }}> Poke</Link></li>
               </ul>
             </li>
             <li >
-              <Link data-id="contacts" className="nav-link mx-3" to="/" onClick={() => { scrollTo('contacts');}}>Contacts</Link>
+              <Link data-id="contacts" className="nav-link mx-3" to="/" onClick={() => { scrollTo('contacts'); closeNavbar(); }}>Contacts</Link>
             </li>
           </ul>
         </nav>
